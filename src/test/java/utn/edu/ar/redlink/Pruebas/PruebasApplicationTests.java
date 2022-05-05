917a468a2307a9510a44f6472e90a5fb97de9f57
Mauro Gustavo Rossi <MR_E91237@redlink.com.ar>
2022-05-05 16:54:30 -0300

    inicio de TP

---
+++ src/test/java/utn/edu/ar/redlink/Pruebas/PruebasApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.edu.ar.redlink.Pruebas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
